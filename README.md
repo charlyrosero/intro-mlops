0e81e56a18ef34859a11df835bc2fd8842563932
Charly Rosero Caldón <charly.rosero@pragma.com.co>
2024-09-20 15:55:21 -0500

    add test api

---
+++ README.md
@@ -0,0 +1,2 @@
+# FastAPI API to AWS ECS Fargate
+![AWS_Fargate_Workflow](./images/fastapi_mlops.png)
---
+++ prueba_enpoint.py
@@ -0,0 +1,19 @@
+import requests
+
+url ="http://35.175.211.120:8000/v1/prediction"
+      
+json = {
+    'opening_gross':8330681,
+    'screens':2271,
+    'production_budget':13000000,
+    'title_year':2019,    
+    'aspect_ratio':1.85,
+    'duration':97,
+    'cast_total_facebook_likes':37907,
+    'budget':16000000,
+    'imdb_score':7.2
+    }
+
+response = requests.post(url=url, json=json)
+
+print(response.json)
